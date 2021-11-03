66a6f03e2388de7c3a2270625590ded9f784b009
Aringco <yuvuy123@naver.com>
2021-11-04 00:00:09 +0900

    UI_Settings,SideBar

--- app/src/main/java/com/example/sss/MapActivity.java
+++ app/src/main/java/com/example/sss/MapActivity.java
@@ -1,10 +1,14 @@
 package com.example.sss;
 
+import androidx.annotation.NonNull;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +21,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback{
     private GoogleMap mMap;
     private ImageButton backBtn;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    private ImageButton sidebarOn;
+    private ImageButton sidebarClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,37 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback{
                 MapActivity.super.onBackPressed();
             }
         });
+
+        // 사이드 바 열기
+        sidebarOn = (ImageButton)findViewById(R.id.sidebar_on);
+        sidebarOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        // 사이드 바 닫기
+        sidebarClose = (ImageButton) findViewById(R.id.sidebar_off);
+        sidebarClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(drawerView);
+            }
+        });
+
+        //sidebar
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer2);
+
+        //drawerLayout.setDrawerListener(listener);
+        drawerLayout.addDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
     }
 
     @Override
@@ -49,4 +88,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback{
         mMap.addMarker(markerOptions);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+            //슬라이드 했을때
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+            //Drawer가 오픈된 상황일때 호출
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+            // 닫힌 상황일 때 호출
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            // 특정상태가 변결될 때 호출
+        }
+    };
 }
