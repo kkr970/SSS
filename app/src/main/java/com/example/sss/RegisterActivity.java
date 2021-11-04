1c53b5edc7adf841c5bd40c8ae52559cbf7a5067
Aringco <yuvuy123@naver.com>
2021-11-04 18:30:05 +0900

    EmailDesign

--- app/src/main/java/com/example/sss/RegisterActivity.java
+++ app/src/main/java/com/example/sss/RegisterActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ImageButton backBtn;
+    private EditText userEmail;
+    private Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //사용자 이메일 칸
+        userEmail = (EditText)findViewById(R.id.userEmail);
 
         backBtn = (ImageButton)findViewById(R.id.backBtn);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +29,24 @@ public class RegisterActivity extends AppCompatActivity {
                 RegisterActivity.super.onBackPressed();
             }
         });
+
+        //이메일 등록 버튼
+        submitBtn = (Button)findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //등록 버튼 이벤트
+                if(submitBtn.getText() == "등록"){
+                    submitBtn.setText("수정");
+                    submitBtn.setBackgroundResource(R.drawable.submit_btn_on);
+                    userEmail.setEnabled(true);
+                } else {
+                    submitBtn.setText("등록");
+                    submitBtn.setBackgroundResource(R.drawable.submit_btn);
+                    userEmail.setEnabled(false);
+                }
+            }
+        });
+
     }
 }
